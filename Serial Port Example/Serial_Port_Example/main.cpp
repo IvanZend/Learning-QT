fb9bf6e20ea5a96fbc6d278d2cd00cb939792a7f
IvanZend <ivanzend42@gmail.com>
2023-07-03 12:34:29 +0400

    int

--- Serial Port Example/Serial_Port_Example/main.cpp
+++ Serial Port Example/Serial_Port_Example/main.cpp
@@ -1,6 +1,8 @@
 #include "widget.h"
 #include <QApplication>
 #include <QMainWindow>
+#include <QAction>
+#include <QToolBar>
 
 int main(int argc, char *argv[])
 {
@@ -8,10 +10,17 @@ int main(int argc, char *argv[])
 
     Widget w;
     QMenuBar menu_bar(&w);
-    QMenu file_menu("file");
-    QMenu settings_menu("settings");
+    QMenu file_menu("File");
+    QMenu settings_menu("Settings");
+    QAction openAct("Open", &file_menu);
+    file_menu.addAction(&openAct);
     menu_bar.addMenu(&file_menu);
     menu_bar.addMenu(&settings_menu);
+
+    QToolBar tool_bar("Tools", &w);
+    QAction open_connection("Connect", &tool_bar);
+    tool_bar.addAction(&open_connection);
+
     w.show();
     return a.exec();
 }
